2e9a1efe4c494dbe528bfcb718e8f39c67b701ca
Ayush Mittal <ayush.mittal@outlook.com>
2020-07-02 23:46:15 +0530

    subscription

--- src/controllers/contact.controller.js
+++ src/controllers/contact.controller.js
@@ -20,12 +20,16 @@ module.exports = {
             return res.status(400).json({ errors: error.errors || error.message });
         }
     },
-
     subscribeNewsletter: async (req, res) => {
         try {
             const { body } = req;
             contactValidation.subscribeNewsletter(body);
             const result = await contactService.subscribeNewsletter(body);
+            awsSesService.sendMail('subscribe.hbs', {
+                toAddresses: [result.email],
+                subject: 'The Trucking Network Subscription',
+                data: { }
+            });
             return res.json({ data: result });
         } catch (error) {
             return res.status(400).json({ errors: error.errors || error.message });
