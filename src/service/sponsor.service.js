991878bba7acb88c4a569320f0d9ccc094edad1a
Nargis <daffolapmac-112@Daffolapmac-112s-MacBook-Air.local>
2020-07-02 15:47:14 +0530

    Sponser image url

--- src/service/sponsor.service.js
+++ src/service/sponsor.service.js
@@ -1,5 +1,7 @@
-const sponsorRepository = require('../repository/sponsor.repository');
+const _ = require('lodash');
 const CustomError = require('../utils/error');
+const sponsorRepository = require('../repository/sponsor.repository');
+const cloudFrontService = require('../service/aws/aws.cloudfront.service');
 
 module.exports = {
     createSponsor: async (data) => {
@@ -8,10 +10,22 @@ module.exports = {
     updateSponsor: async (sponsorId, data) => {
         // const sponsor = await sponsorRepository.findOne({ _id: sponsorId })
         // if (!sponsor) throw new CustomError(404, 'Sponsor not found');
-        return sponsorRepository.findByIdAndUpdate(sponsorId, data); 
+        return sponsorRepository.findByIdAndUpdate(sponsorId, data);
     },
     getSponsors: async () => {
-        return sponsorRepository.find({ is_active: true });
+        let sponsers = await sponsorRepository.find({ is_active: true });
+        if (!_.isEmpty(sponsers)) {
+            sponsers = _.map(sponsers, sponser => {
+                return sponser.toObject();
+            });
+            const imagePromise = sponsers.map(async (item) => {
+                item.image_url = await cloudFrontService.getSignedUrl(item.image)
+                return item;
+            });
+            sponsers.image_url = await Promise.all(imagePromise);
+        }
+
+        return sponsers;
     },
     deleteSponsor: async (sponsorId) => {
         const sponsor = await sponsorRepository.findOne({ _id: sponsorId })
